34dab28c9cc7346eccc0ce5e36a1b282f80fcac7
Ilya Albrekht <ilya.albrekht@intel.com>
2016-07-13 11:06:41 -0700

    cache ical files

--- src/main.cpp
+++ src/main.cpp
@@ -48,23 +48,13 @@
 // Time after which to check if any of the valves are active (sec)
 #define CHECK_TIME 10
 // Time to update calendar info from the internet (sec)
-#define UPDATE_TIME (60*60*10)
+#define UPDATE_TIME (60*60*4)
 
 //// TODO List
 // * Extract cal for a week
 
 using namespace std;
 using namespace LibICal;
-
-// Type declaration
-struct valve_t
-{
-    iCalValveControl vc;
-    GpioRelay gpio;
-    const string url;
-
-    valve_t(string ical_url, int gpio_pin) : gpio(gpio_pin), vc(GT_DEFUALT), url(ical_url) {}
-};
 
 // Global variables
 int g_DebugLevel=6;
@@ -130,12 +120,7 @@ int main(int argc, char* argv[])
     signal(SIGTERM, sighandler_stop);
 
 
-    // Start main app loop
-    // Should go to deamon mode at this point
-
-    //iCalValveControl vc(GT_DEFUALT);
-    //GpioRelay R1(PIN_V1);
-
+    // Load configuration and create 
     {
         ifstream cf("config.json");
         string config_str((std::istreambuf_iterator<char>(cf)),
@@ -165,65 +150,39 @@ int main(int argc, char* argv[])
                 string gpio_id = root["schedule"][i]["igpo"][0];
                 shared_ptr<valve_t> vt(new valve_t(root["schedule"][i]["url"],std::stoi(gpio_id)) );
 
-                char f_name[255];
-                sprintf(f_name, "%d.ics", vt->gpio.GetPin());
-                vt->vc.ParseICALFromFile(f_name);
+                cout << "$ filename " << vt->get_cache_filename() << endl;
+                if(!vt->load_cached()) {
+                    cout << "Load from web\n";
+                    vt->load_from_url();
+                }
                 valves.push_back(vt);
             }
         }
     }
 
-    /*{
-        shared_ptr<valve_t> vt(new valve_t("https://calendar.google.com/calendar/ical/04n0submlvfodumeo7ola6f90s%40group.calendar.google.com/private-b40357d4bfaee14d76ffaa65e910d554/basic.ics",
-            PIN_V1) );
-        char f_name[255];
-        sprintf(f_name, "%d.ics", vt->gpio.GetPin());
-        vt->vc.ParseICALFromFile(f_name);
-
-        valves.push_back(vt);
-    }*/
-
-    // vc.ParseICALFromFile("./7.ics");
     time_t last_reload = time(0); //-2*UPDATE_TIME; // Hack to force the reload on the first iteration
 
     // Enter into work loop
     while(true)
     {
+        bool to_reload;
+        if( to_reload = (time(0) - last_reload) > UPDATE_TIME )
+            last_reload = time(0);
         for( auto &vt : valves)
         {
-            if( (time(0) - last_reload) > UPDATE_TIME )    // do it every UPDATE_TIME seconds
+            if(to_reload)
             {
-                string ical_string("");
-                // string URL("https://calendar.google.com/calendar/ical/04n0submlvfodumeo7ola6f90s%40group.calendar.google.com/private-b40357d4bfaee14d76ffaa65e910d554/basic.ics");
                 int err;
-
-                if( (err = load_ical_from_url(ical_string, vt->url, vt->vc.LastUpdated())) == NET_SUCCESS)
-                {
-                    vt->vc.ParseICALFromString(ical_string);
-
-                    // TODO - store to $ file
-                }
-                else if(err == NET_NOT_CHANGED)
-                {
-                    //TODO force reload from the cache once a day to generate schedule for 7 days
-                    //      and store it in the cache
-                }
-                else if(err < 0)
-                {
+                if(err=vt->load_from_url() !=NET_SUCCESS) {
                     DEBUG_PRINT(LOG_INFO, "ERROR: Error loading ical" );
                 }
-
-                // FIXME ?!?
-                last_reload = time(0); // is it better to update the time before or after the load??
             }
 
             DEBUG_PRINT(LOG_INFO, "INFO: update status");
-            //set_valve_status(0, vc.IsActive());
             if(vt->vc.IsActive() != vt->gpio.GetStatus())
                 vt->gpio.SetStatus(vt->vc.IsActive());
         }
 
-        // TODO adjust sleep time for schedule update
         sleep(CHECK_TIME);
     }
     
--- src/valve.cpp
+++ src/valve.cpp
@@ -1,6 +1,70 @@
 #include "valve.h"
+#include "net.h"
 
 #include <stdio.h>
+#include <string>
+#include <cyassl/ctaocrypt/md5.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+
+
+string get_cache_filename();
+
+std::string valve_t::get_hash()
+{
+
+	byte md5sum[MD5_DIGEST_SIZE];
+	char r[MD5_DIGEST_SIZE*2+1];
+
+	Md5 md5;
+	InitMd5(&md5);
+
+	Md5Update(&md5, (const byte *)this->url.c_str(), this->url.length());
+	Md5Final(&md5, md5sum);
+
+	r[MD5_DIGEST_SIZE*2] = 0;
+	for(int i=0;i<MD5_DIGEST_SIZE;++i)
+		sprintf(&r[2*i],"%02X",md5sum[i]);
+	return r;
+}
+
+std::string valve_t::get_cache_filename()
+{
+	return "./"+this->get_hash()+".ics";
+}
+
+bool valve_t::load_cached()
+{
+	// check if $ file exists
+	struct stat st;
+	std::string f_name = this->get_cache_filename();
+	if(stat(f_name.c_str(),&st) == 0)
+	{
+		this->vc.ParseICALFromFile(f_name);
+		return true;
+	}
+	else
+		return false;
+}
+
+int valve_t::load_from_url(bool store_to_cache)
+{
+	std::string ical_string="";
+	int err;
+	if( (err = load_ical_from_url(ical_string, this->url, 0)) == NET_SUCCESS)
+	{
+	    this->vc.ParseICALFromString(ical_string);
+	    if(store_to_cache) {
+	    	ofstream f(this->get_cache_filename());
+	    	f << ical_string;
+	    	f.close();
+	    }
+	}
+	return err;
+}
 
 int set_valve_status(unsigned int id, bool active)
 {
