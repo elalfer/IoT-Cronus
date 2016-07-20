20fc6ceaf6e05ffe17ba3a0e606370312ab48ba1
Ilya Albrekht <ilya@alfs.me>
2016-07-20 19:56:00 +0000

    Edison gpio fixe

--- src/gpio_edison.cpp
+++ src/gpio_edison.cpp
@@ -1,20 +1,28 @@
 #include "gpio.h"
+#include "log.h"
 
 #include <stdlib.h>
 #include <mraa/gpio.hpp>
 
 GpioRelay::GpioRelay(int GpioPIN)
 {
-
 	pin_id=GpioPIN;
 	is_on=false;
+
+	//mraa_init();
+	//mraa_gpio_context c = mraa_gpio_init(GpioPIN);
+	//mraa_gpio_write(c,1);
+
+	//return;
 
 	mraa::Gpio* gpio = new mraa::Gpio(GpioPIN);
 	if (!gpio) {
 		fprintf(stderr, "ERROR [Fatal]: can not connect to gpio port %d\n", GpioPIN);
 		exit(-1);
 	}
+	// gpio->mode(mraa::MODE_PULLDOWN);
 	mraa::Result response = gpio->dir(mraa::DIR_OUT);
+	//gpio->dir(mraa::DIR_OUT);
 	if (response != mraa::SUCCESS) {
 		// mraa::printError(response);
 		fprintf(stderr, "ERROR [Fatal]: can not connect to gpio port %d\n", GpioPIN);
@@ -22,6 +30,7 @@ GpioRelay::GpioRelay(int GpioPIN)
 	}
 
 	this->handler = gpio;
+	this->SetStatus(false);
 }
 
 GpioRelay::~GpioRelay()
@@ -30,13 +39,14 @@ GpioRelay::~GpioRelay()
 	DEBUG_PRINT(LOG_INFO, "Delete Gpio device #" << this->pin_id);
 
 	delete static_cast<mraa::Gpio*>(this->handler);
+	// mraa_deinit();
 }
 
 int GpioRelay::SetStatus(bool on)
 {
 	DEBUG_PRINT(LOG_INFO, "Set pin #" << this->pin_id << " state to " << on );
 
-	static_cast<mraa::Gpio*>(this->handler)->write(on?1:0);
+	static_cast<mraa::Gpio*>(this->handler)->write(on?0:1);
 	is_on = on;
 }
 
