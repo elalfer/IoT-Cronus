1774c1f6bab506987b0b10295296690fd2312e5c
Ilya Albrekht <ilya.albrekht@intel.com>
2016-07-23 12:59:46 -0700

    output schedule under --debug YCM config script

--- src/log.h
+++ src/log.h
@@ -5,8 +5,28 @@
 #include <syslog.h>
 #include <iostream>
 
+#include <time.h>
+#include <string.h>
+
 extern int g_DebugLevel;
 
 #define DEBUG_PRINT(log,txt) if(log <= g_DebugLevel) std::cout << log << ": " << txt << " (" << __PRETTY_FUNCTION__ << ")" <<  std::endl;
+
+static char *format_time_t(time_t t)
+{
+	char *b;
+	b = ctime(&t);
+	b[strlen(b)-1]='\0';
+
+	return b;
+}
+
+static char *format_timespan_t(time_t t1, time_t t2)
+{
+	static char ts[256];
+	sprintf(ts, "%s", format_time_t(t1));
+	sprintf(ts, "%s - %s", ts, format_time_t(t2));
+	return ts;
+}
 
 #endif
