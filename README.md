f29d72450e7f7a9e9798e4880ed7461b3cae89d7
Ilya Albrekht <ilya@alfs.me>
2016-08-31 10:45:13 -0700

    Added Apache 2.0 License headers and file

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # IoT-Sheduler
 
-Simple project to schedule tasks on IoT device using local or web hosted iCal files
+Simple project to schedule tasks on IoT device using local or web hosted iCal files, open sourced under the Apache 2.0 license.
+
+# Installation
+
+Third party libraries:
+ * ArduinoJson https://github.com/bblanchon/ArduinoJson
+ * WolfSSL https://www.wolfssl.com/
+ * lib iCal http://libical.github.io/libical/
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,16 @@
+// Copyright 2016 Ilya Albrekht
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-//  main.cpp
-//  
+//    http://www.apache.org/licenses/LICENSE-2.0
 //
-//  Created by Ilya Albekht on 11/29/15.
-//
-//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #include <stdio.h>
 #include <stdlib.h>
